8248c915c5285b8801e5ebe447a16fe11246a22a
Maksim Peskov <kostarsf@gmail.com>
2022-12-21 21:41:02 +0300

    Update api key form

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,8 @@ function App() {
     return () => clearInterval(newTimeInterval)
   }, [apiKey])
 
-  const useApiKeyHandle = () => {
+  const useApiKeyHandle = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     const newKey = String(apiKeyFieldRef.current?.value)
     setApiKey(newKey)
     localStorage.setItem("livapp-apikey", newKey)
@@ -71,16 +72,13 @@ function App() {
 
   return (
     <div className="App">
-      {errorMessage && (
-      <>
-        <form className='flex mb-3 bg-white shadow'>
-          <input className='flex-1 w-0 bg-white px-2' name='api_key' type="text" id="api_key_field" placeholder="API ключ" ref={apiKeyFieldRef}/>
-          <input className='font-medium bg-slate-300 px-3 py-1 cursor-pointer hover:bg-slate-700 hover:text-white' type="button" value="Применить ключ" id="api_key_apply_btn" onClick={useApiKeyHandle}/>
-        </form>
-        <div>
-          <p>{errorMessage}</p>
-        </div>
-      </>)}
+      <form className='flex mb-3 bg-white shadow' onSubmit={useApiKeyHandle}>
+        <input className='flex-1 w-0 bg-white px-2' name='api_key' type="text" id="api_key_field" placeholder="API ключ" ref={apiKeyFieldRef}/>
+        <input className='font-medium bg-slate-300 px-3 py-1 cursor-pointer hover:bg-slate-700 hover:text-white' type="submit" value="Применить ключ" id="api_key_apply_btn"/>
+      </form>
+      <div>
+        <p>{errorMessage}</p>
+      </div>
       <div className='flex flex-col gap-3'>
         {systems.map(system => {
           const updateDate = new Date(system.last_online)
